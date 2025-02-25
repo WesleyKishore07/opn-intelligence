6cd7a80784f19879764bd6aea550722b85c03ba3
WesleyKishore07 <wesley@orchestro.ai>
2025-02-25 16:06:32 +0530

    f

--- app_services_parent/settlement/app/main.py
+++ app_services_parent/settlement/app/main.py
@@ -1,4 +1,4 @@
-# app-services-parent/settlement/app/main.py
+# app_services_parent/settlement/app/main.py
 from fastapi import FastAPI, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 from common.config.base_config import get_service_port
